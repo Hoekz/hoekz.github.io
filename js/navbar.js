a523d742a7e4559cf749e17771f7359c2b51c390
Hoekz <jphx48@mst.edu>
2014-10-07 16:17:36 -0500

    again
    fuuuuuuuuuuu-

--- js/navbar.js
+++ js/navbar.js
@@ -1,8 +1,8 @@
 app.controller('navbar',['$scope', '$location', function($scope, $location){
     $scope.menu = [
-        {name: 'Home', link: ''},
-        {name: 'About', link: 'about'},
-        {name: 'Projects', link: 'projects'}
+        {name: 'Home', link: '%2F'},
+        {name: 'About', link: '%2Fabout'},
+        {name: 'Projects', link: '%2Fprojects'}
     ];
     $scope.$on('$routeChangeSuccess', function(){
         $scope.loc = $location.path();
