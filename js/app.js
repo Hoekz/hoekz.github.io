af7d56e4e4ae338dfad1db765bae8e690e8f6f5b
Hoekz <jphx48@mst.edu>
2014-10-07 00:58:07 -0500

    CSS and Content Update
    Yup

--- js/app.js
+++ js/app.js
@@ -3,19 +3,19 @@ var app = angular.module('app',['ngRoute']);
 app.config(['$routeProvider', '$locationProvider', function($routeProvider, loc){
     $routeProvider
         .when('/',{
-            templateUrl: '/html/home.html',
+            templateUrl: 'html/home.html',
             controller: 'home'
         })
         .when('/about',{
-            templateUrl: '/html/about.html',
+            templateUrl: 'html/about.html',
             controller: 'about'
         })
         .when('/projects',{
-            templateUrl: '/html/projects.html',
+            templateUrl: 'html/projects.html',
             controller: 'projects'
         })
         .when('/projects/:project',{
-            templateUrl: '/html/project.html',
+            templateUrl: 'html/project.html',
             controller: 'project'
         })
         .otherwise({redirectTo: '/'});
