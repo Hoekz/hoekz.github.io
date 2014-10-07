485ea9a50de28f122c7864484990c99d281878c7
Hoekz <jphx48@mst.edu>
2014-10-06 19:06:28 -0500

    Basic Commit
    Added app.js for routing, home.html to start home page, etc.

---
+++ js/app.js
@@ -0,0 +1,22 @@
+var app = angular.module('app',['ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider){
+    $routeProvider
+        .when('/',{
+            templateUrl: 'html/home.html',
+            controller: 'home'
+        })
+        .when('/about',{
+            templateUrl: 'html/about.html',
+            controller: 'about'
+        })
+        .when('/projects',{
+            templateUrl: 'html/projects.html',
+            controller: 'projects'
+        })
+        .when('/projects/:project',{
+            templateUrl: 'html/project.html',
+            controller: 'project'
+        })
+        .otherwise({redirectTo: '/'});
+}]);
