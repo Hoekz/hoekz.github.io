de0ed6aa65fc6fc27f1cda4b4e9fc1390c44d9e9
Hoekz <jphx48@mst.edu>
2014-10-11 13:31:49 -0500

    Hopefully fixes pages
    because they were...eh

--- js/fetch.js
+++ js/fetch.js
@@ -20,16 +20,10 @@ app.factory('$fetch', ['$http', '$rootScope', function($http, $root){
         }else{
             $http.get('json/about.json').success(function(data){
                 self.about = data;
-                var getNext = function(i){
-                    $http.get(self.about.sections[i].content).success(function(data){
-                        self.about.sections[i].content = data.split('\n');
-                        if(i == self.about.sections.length - 1)
-                            $root.$broadcast('about');
-                        else
-                            getNext(i + 1);
-                    });
-                };
-                getNext(0);
+                $http.get(self.about.bio.content).success(function(data){
+                    self.about.bio.content = data;
+                    $root.$broadcast('about');
+                });
             });
             return false;
         }
